f174883b387b8c7a4b34872f148e7b4d4b0d3891
DawitMulugeta23 <dawitmulugetas23@gmail.com>
2026-03-31 02:38:00 +0300

    updated

--- frontend/src/context/CartContext.jsx
+++ frontend/src/context/CartContext.jsx
@@ -1,6 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useContext, useState } from "react";
 
-const CartContext = createContext();
+export const CartContext = createContext();
+
+export const useCart = () => {
+  const context = useContext(CartContext);
+
+  if (!context) {
+    throw new Error("useCart must be used within a CartProvider");
+  }
+
+  return context;
+};
 
 export const CartProvider = ({ children }) => {
   const [cartItems, setCartItems] = useState([]);
