92ec6e741fbbed4918ec71553c11d01b55db5657
DawitMulugeta23 <dawitmulugetas23@gmail.com>
2026-04-10 22:45:35 +0300

    cors updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -45,6 +45,7 @@ app.use(passport.session());
 // CORS configuration
 const corsOptions = {
   origin: [
+    "https://future-fs-03-wine-chi.vercel.app/",
     "http://localhost:5173",
     "http://localhost:3000",
     "http://127.0.0.1:5173",
